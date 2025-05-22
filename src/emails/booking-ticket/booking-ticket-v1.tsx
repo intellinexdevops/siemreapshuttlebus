caa737f072f78705b1c7897b434129da7f0a4077
Chenter <chenterphai61@gmail.com>
2025-05-22 19:06:17 +0700

    ..

--- src/emails/booking-ticket/booking-ticket-v1.tsx
+++ src/emails/booking-ticket/booking-ticket-v1.tsx
@@ -29,8 +29,8 @@ interface EmailTemplateProps {
 const colorPrimary = "#1C6AE4";
 const colorText = "#262626";
 
-const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL
-// const imageUrl = "http://localhost:3000/"
+// const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL
+const imageUrl = "https://siemreapshuttlebus.netlify.app/"
 
 export default async function EmailTemplate({
     name = "Chenter",
@@ -45,16 +45,7 @@ export default async function EmailTemplate({
     return (
         <Html>
             <Head>
-                <Font
-                    fontFamily="Poppins"
-                    fallbackFontFamily="Arial"
-                    webFont={{
-                        url: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
-                        format: "woff2",
-                    }}
-                    fontWeight={400}
-                    fontStyle="normal"
-                />
+                <Font fontFamily="Arial" fallbackFontFamily="Arial" />
             </Head>
 
             <Body>
@@ -300,7 +291,7 @@ export default async function EmailTemplate({
 
 
 const header = {
-    backgroundImage: 'url("http://localhost:3000/email-bg.svg")',
+    backgroundImage: `url("${imageUrl}email-bg.svg")`,
     padding: 16
 }
 
