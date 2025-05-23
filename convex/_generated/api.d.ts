66ed45537328c8b0788c19c1210981a836c4e71b
Chenter <chenterphai61@gmail.com>
2025-05-23 14:35:37 +0700

    Modified

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as airplane_time from "../airplane_time.js";
 import type * as tasks from "../tasks.js";
 import type * as transportation from "../transportation.js";
 
@@ -25,6 +26,7 @@ import type * as transportation from "../transportation.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  airplane_time: typeof airplane_time;
   tasks: typeof tasks;
   transportation: typeof transportation;
 }>;
